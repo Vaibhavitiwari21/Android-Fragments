7e5f295f42c09080f22d978d95a909121dbc45ca
Vaibhavitiwari21 <tiwarivaibhavi2103@gmail.com>
2023-06-21 10:32:01 -0400

    Initial commit

---
+++ app/src/main/java/com/example/fragments/fragment_fragment1.java
@@ -0,0 +1,14 @@
+package com.example.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fragment_fragment1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_fragment1);
+    }
+}
